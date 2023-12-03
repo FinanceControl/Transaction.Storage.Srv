ba64ec5cfe60973fb313f422248ed04d5deef435
InsonusK <insonus.k@gmail.com>
2023-12-03 17:15:02 +0000

    update validator

--- src/App/Transaction.Storage.Srv.App.Core.Aggregates.TransactionAggregate/Models/Position.Validators.cs
+++ src/App/Transaction.Storage.Srv.App.Core.Aggregates.TransactionAggregate/Models/Position.Validators.cs
@@ -11,22 +11,24 @@ public partial class Position
 {
   public class Validator : AbstractValidator<IPositionBodyDto>
   {
-    public Validator(IReadRepositoryBase<Account> accountReadRep, IReadRepositoryBase<Asset> assetReadRep) : this()
+    public Validator(IReadRepositoryBase<Account> accountReadRep, IReadRepositoryBase<Asset> assetReadRep)
     {
       RuleFor(at => at.AccountId).SetValidator(new IdNullableValidator<Account>(accountReadRep, nameof(AccountId)));
       RuleFor(at => at.AssetId).SetValidator(new IdValidator<Asset>(assetReadRep, nameof(AssetId)));
-    }
-    public Validator()
-    {
-      RuleFor(e => e.Amount).SetValidator(new AmountValidator());
+      RuleFor(e => e.Amount).SetValidator(at => new AmountValidator(assetReadRep, at.AssetId));
     }
   }
 
-  public class AmountValidator : AbstractValidator<int>
+  public class AmountValidator : AbstractValidator<decimal>
   {
-    public AmountValidator()
+    public AmountValidator(IReadRepositoryBase<Asset> assetReadRep, int assetId)
     {
-      RuleFor(e => e).NotEqual(0).WithMessage("Amount counln't be 0");
+      var res =
+      RuleFor(e => e)
+        .NotEqual(0).WithMessage("Amount counln't be 0")
+        .WhenAsync(async (e, ct) => await assetReadRep.GetByIdAsync(assetId, ct) != null)
+          .MustAsync(async (e, ct) => (await assetReadRep.GetByIdAsync(assetId, ct))!.checkDecimalLenght(e).IsSuccess)
+          .WithMessage("Decimal lenght is too long for Asset");
     }
   }
 }
